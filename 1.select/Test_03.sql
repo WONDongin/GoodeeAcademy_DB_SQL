b133551def4f5fb6dce78315adc20619b8cebad8
WONDongin <88768560+WONDongin@users.noreply.github.com>
2025-03-09 22:58:49 +0900

    4장. join_Refactoring
    join_Refactoring

--- 1.select/Test_03.sql
+++ 1.select/Test_03.sql
@@ -9,10 +9,17 @@ SELECT CODE,NAME,build, IFNULL(build,"단독 건물 없음") FROM major;
 -- 3. 학생의 이름, 전화번호, 지역명 조회하기
 --    지역명 : 지역번호가 02 : 서울, 031:경기, 032:인천 그외 기타지역
 SELECT NAME AS 이름,tel AS "전화번호",
-	case  when SUBSTR(tel,1,INSTR(tel,')')-1) = '02' then "서울" 
-			when SUBSTR(tel,1,INSTR(tel,')')-1) = '031' then "경기" 
-			when SUBSTR(tel,1,INSTR(tel,')')-1) = '032' then "인천" 
-			ELSE "그외 기타지역" END AS "지역명"
+	case  SUBSTR(tel,1,INSTR(tel,')')-1)
+	when '02' then "서울" 
+	when '031' then "경기" 
+	when '032' then "인천" 
+	ELSE "그외 기타지역" END AS "지역명"
+FROM student;
+
+SELECT NAME AS 이름,tel AS "전화번호", 
+if(SUBSTR(tel,1,INSTR(tel,')')-1) = '02','서울',
+if(SUBSTR(tel,1,INSTR(tel,')')-1) = '031','경기',
+if(SUBSTR(tel,1,INSTR(tel,')')-1) = '032','인천', "그외 기타지역" ))) AS '지역명'
 FROM student;
 
 -- 4. 학생의 이름, 전화번호, 지역명 조회하기
@@ -20,6 +27,10 @@ FROM student;
 SELECT NAME AS "이름",tel AS "전화번호",
 	case  when SUBSTR(tel,1,INSTR(tel,')')-1) IN (02,031,032) then "수도권"
 			ELSE "그외 기타지역" END AS "지역명"
+FROM student;
+
+SELECT NAME AS "이름",tel AS "전화번호",
+	if(LEFT(tel, INSTR(tel, ')')-1) IN('02','031','032'), '수도권','그외기타지역')
 FROM student;
 
 -- 5. 학생을 3개 팀으로 분류하기 위해 학번을 3으로 나누어 
@@ -41,16 +52,16 @@ FROM student;
 --                         65이상이면 D+,60 이상 D0
 --    인정여부는 평균이 60이상이면 PASS로 미만이면 FAIL로 출력한다.                   
 --    으로 출력한다.
-SELECT studno,kor,eng,math,if(ROUND((kor+eng+math)/3,0) >= 60, "PASS","FAIL") AS "인정여부" , (kor+eng+math)/3,
+SELECT *, if(ROUND((kor+eng+math)/3,0) >= 60, "PASS","FAIL") AS "인정여부",
 	case  when ROUND((kor+eng+math)/3,0) >= 95 then "A+"
-			when ROUND((kor+eng+math)/3,0) BETWEEN 90 AND 94 then "A0"
-			when ROUND((kor+eng+math)/3,0) BETWEEN 85 AND 89 then "B+"
-			when ROUND((kor+eng+math)/3,0) BETWEEN 80 AND 84 then "B0"
-			when ROUND((kor+eng+math)/3,0) BETWEEN 75 AND 79 then "C+"
-			when ROUND((kor+eng+math)/3,0) BETWEEN 70 AND 74 then "C0"
-			when ROUND((kor+eng+math)/3,0) BETWEEN 65 AND 69 then "D+"
-			when ROUND((kor+eng+math)/3,0) BETWEEN 60 AND 64 then "D0"
-			ELSE "" END AS "학점"
+			when ROUND((kor+eng+math)/3,0) >= 94 then "A0"
+			when ROUND((kor+eng+math)/3,0) >= 85 then "B+"
+			when ROUND((kor+eng+math)/3,0) >= 80 then "B0"
+			when ROUND((kor+eng+math)/3,0) >= 75 then "C+"
+			when ROUND((kor+eng+math)/3,0) >= 70 then "C0"
+			when ROUND((kor+eng+math)/3,0) >= 65 then "D+"
+			when ROUND((kor+eng+math)/3,0) >= 60 then "D0"
+			ELSE "F" END AS "학점"
 FROM score;    
 
 -- 7. 학생 테이블에서 이름, 키, 키의 범위에 따라 A, B, C ,D그룹을 출력하기
@@ -59,22 +70,20 @@ FROM score;
 --    170 ~ 179까지 : C그룹
 --    180이상       : D그룹
 SELECT NAME,height,
-	case  when height < 160 then "A그룹"
-			when height BETWEEN 160 AND 169 then "B그룹"
-			when height BETWEEN 170 AND 179 then "C그룹"
-			when height >= 180 then "D그룹"
-			ELSE "" END AS "키의 범위"
+	case  when height >= 180 then "D그룹" 
+			when height >= 170 then "C그룹" 
+			when height >= 160 then "B그룹"
+			ELSE "A그룹" END AS "키의 범위"
 FROM student;
 
 -- 8. 교수테이블에서 교수의 급여액수를 기준으로 200이하는 4급, 201~300 : 3급, 301~400:2급
 --    401 이상은 1급으로 표시한다. 교수의 이름, 급여, 등급을 출력하기
 --    단 등급의 오름차순으로 정렬하기
 SELECT NAME,salary,
-	case  when salary <= 200 then "4급"
-			when salary BETWEEN 201 AND 300 then "3급"
-			when salary BETWEEN 301 AND 400 then "2급"
-			when salary >= 410 then "1급"
-			ELSE "" END AS "등급"
+	case  when salary >= 410 then "1급"
+			when salary >= 301 then "2급"
+			when salary >= 201 then "3급"			
+			ELSE "4급" END AS "등급"
 FROM professor
 ORDER BY "등급";
 
@@ -89,41 +98,92 @@ ORDER BY grade;
 --     가장 키가 큰키와, 가장 작은키, 키의 평균을 구하기 
 SELECT major1 AS "전공1학과", MAX(height) AS "가장 키가 큰키", MIN(height) AS "가장 작은 키", AVG(height) AS "키의 평균" FROM student
 GROUP BY major1
-HAVING AVG(height) >= 170  
+HAVING AVG(height) >= 170;  
 
 -- 11. 사원의 직급(job)별로 평균 급여를 출력하고, 
 --     평균 급여가 1000이상이면 '우수', 작으면 '보통'을 출력하여라
 SELECT job AS "직급", ROUND(AVG(salary),0) AS "평균 급여", if(ROUND(AVG(salary),0) >= 1000, "우수","보통") AS "등급" FROM emp
-GROUP BY job  
+GROUP BY job;  
 
 -- 12. 학과별로 학생의 평균 몸무게와 학생수를 출력하되 
 --     평균 몸무게의 내림차순으로 정렬하여 출력하기
-SELECT major1 AS "학과", COUNT(*) AS "학생수", ROUND(AVG(weight),0) AS "평균 몸무게"  FROM student
+SELECT major1 AS "학과", ROUND(AVG(weight),0) AS "평균 몸무게", COUNT(*) AS "학생수"  FROM student 
 GROUP BY major1
 ORDER BY ROUND(AVG(weight),0) DESC;
 
 -- 13. 학과별 교수의 수가 2명 이하인 학과번호, 인원수를 출력하기
+SELECT deptno, COUNT(*)
+FROM professor 
+GROUP BY deptno
+HAVING COUNT(*) <= 2;
 
 -- 14. 전화번호의 지역번호가 02서울 031 경기, 051 부산, 052 경남, 나머지 그외지역
 --     학생의 인원수를 조회하기
+SELECT 
+	case LEFT(tel,INSTR(tel,')')-1) when '02' then '서울'
+	when '031' then '경기'
+	when '051' then '부산'
+	when '052' then '경남'
+	ELSE '그외지역' END AS 지역번호
+	,COUNT(*)
+FROM student
+GROUP BY 지역번호;
 
 -- 15. 전화번호의 지역번호가 02서울 031 경기, 051 부산, 052 경남, 나머지 그외지역
 --     학생의 인원수를 조회하기. 가로출력
+SELECT CONCAT(COUNT(*)+"", "명") AS '총합',
+	SUM(if(LEFT(tel,INSTR(tel,')')-1) = '02',1,0)) AS '서울',
+	SUM(if(LEFT(tel,INSTR(tel,')')-1) = '031',1,0)) AS '경기',
+	SUM(if(LEFT(tel,INSTR(tel,')')-1) = '051',1,0)) AS '부산',
+	SUM(if(LEFT(tel,INSTR(tel,')')-1) = '052',1,0)) AS '경남',
+	SUM(if(LEFT(tel,INSTR(tel,')')-1) not IN ('02','031','051','052'),1,0)) AS '그외지역'
+FROM student;
 
 -- 16. 교수들의 번호,이름,급여,보너스, 총급여(급여+보너스)
 --     급여많은순위,보너스많은순위,총급여많은 순위 조회하기
 --     총급여순위로 정렬하여 출력하기. 보너스없는 경우 0으로 함
+SELECT NO 번호,NAME 이름,salary 급여,IFNULL(bonus,0) 보너스,salary+IFNULL(bonus,0) 총급여, 
+RANK() OVER(ORDER BY salary DESC) 급여많은순위, 
+RANK() OVER(ORDER BY IFNULL(bonus,0) DESC) 보너스많은순위,
+RANK() OVER(ORDER BY salary+IFNULL(bonus,0) DESC) 총급여많은순위
+FROM professor
+ORDER BY salary+IFNULL(bonus,0) DESC;
 
 -- 17. 교수의 직급,직급별 인원수,급여합계,보너스합계,급여평균,보너스평균 출력하기
 --     단 보너스가 없는 교수도 평균에 포함되도록 한다.
 --     급여평균이 높은순으로 정렬하기
+SELECT POSITION, COUNT(*) 인원수, SUM(salary) 급여합계, SUM(ifnull(bonus,0)) 보너스합계, ROUND(AVG(salary)) 급여평균, ROUND(AVG(ifnull(bonus,0))) 보너스평균
+FROM professor
+GROUP BY POSITION
+ORDER BY ROUND(AVG(salary)) DESC;
 
 -- 18. 1학년 학생의 인원수,키와 몸무게의 평균 출력하기
+SELECT COUNT(*) 인원수, ROUND(AVG(height)) 키평균, ROUND(AVG(weight)) 몸무게평균
+FROM student
+WHERE grade = '1';
 
 -- 19. 학생의 점수테이블(score)에서 수학 평균,수학표준편차,수학분산 조회하기
-
+SELECT AVG(math), STDDEV(math), VARIANCE(math) 
+FROM score 
 -- 20. 교수의 월별 입사 인원수를 출력하기
+SELECT CONCAT(MONTH(hiredate), "월") 월별, COUNT(*) 인원수
+FROM professor
+GROUP BY MONTH(hiredate);
 
+SELECT CONCAT(COUNT(*)+"", "명") 전체,
+SUM(if(MONTH(hiredate) = 1,1,0)) '1월',
+SUM(if(MONTH(hiredate) = 2,1,0)) '2월',
+SUM(if(MONTH(hiredate) = 3,1,0)) '3월',
+SUM(if(MONTH(hiredate) = 4,1,0)) '4월',
+SUM(if(MONTH(hiredate) = 5,1,0)) '5월',
+SUM(if(MONTH(hiredate) = 6,1,0)) '6월',
+SUM(if(MONTH(hiredate) = 7,1,0)) '7월',
+SUM(if(MONTH(hiredate) = 8,1,0)) '8월',
+SUM(if(MONTH(hiredate) = 9,1,0)) '9월',
+SUM(if(MONTH(hiredate) = 10,1,0)) '10월',
+SUM(if(MONTH(hiredate) = 11,1,0)) '11월',
+SUM(if(MONTH(hiredate) = 12,1,0)) '12월'
+FROM professor;
 /*
 단일행 함수
 	기타함수
